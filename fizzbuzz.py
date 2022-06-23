d71455e2b32c2500a2c6a9715819ef473189d711
zinzero0 <wlsdud9979@gmail.com>
2022-06-23 18:01:18 +0900

    Feat : if i is times of 3 , print fizzi

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,11 @@
 #3의 배수일때 :fizz
 #5의 배수일때 :buzz
 #15의 배수일때:fizzbuzz
-#나머지는 숫자로 출력
+#나머지는 숫자로 출력 (done)
 
 for i in range(1,46):
-    print(i)
+    
+    if i%3 == 0:
+        print('fizz')
+    else: 
+        print(i)
