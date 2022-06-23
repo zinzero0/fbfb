3b2a17f1904e51bb24131d429a5bffe882aae5db
zinzero0 <wlsdud9979@gmail.com>
2022-06-23 20:24:23 +0900

    feat : fizzbuzz done

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,12 @@
 #3의 배수일때 :fizz(done)
-#5의 배수일때 :buzz
+#5의 배수일때 :buzz(done)
 #15의 배수일때:fizzbuzz
 #나머지는 숫자로 출력 (done)
 
 for i in range(1,46):
-    
-    if i%3 == 0:
+    if i%15 == 0:
+        print('fizzbuzz')
+    elif i%3 == 0:
         print('fizz')
     elif i%5 == 0:
         print('buzz')
